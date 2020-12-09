acea7a72ab1b580c29283915f307b806a55392bf
DAChenScratch <73948853+DAChenScratch@users.noreply.github.com>
2020-12-09 14:33:49 -0600

    Create maths.py

---
+++ maths.py
@@ -0,0 +1,12 @@
+import math
+def sqr(x):
+  return x * x
+def distance_between(one, two):
+  return math.sqrt(sqr(two["x"] - one["x"]) + sqr(two['y'] - one['y']))
+
+def manhattan(one, two):
+  return abs(two["x"] - one["x"]) + abs(two['y'] - one['y'])
+
+
+def roundto(x, roundthis):
+  return round(x/roundthis) * roundthis
